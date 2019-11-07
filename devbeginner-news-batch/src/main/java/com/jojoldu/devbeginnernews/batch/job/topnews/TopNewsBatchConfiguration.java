32e8ca57995de0dd2718121aa67beec24d1ba160
이동욱 <jojoldu@gmail.com>
2019-11-07 22:10:47 +0900

    git cache rm

--- devbeginner-news-batch/src/main/java/com/jojoldu/devbeginnernews/batch/job/topnews/TopNewsBatchConfiguration.java
+++ devbeginner-news-batch/src/main/java/com/jojoldu/devbeginnernews/batch/job/topnews/TopNewsBatchConfiguration.java
@@ -6,8 +6,8 @@ import com.jojoldu.devbeginnernews.batch.job.facebook.feed.FacebookPageItemReade
 import com.jojoldu.devbeginnernews.batch.job.facebook.feed.dto.FacebookFeedDto;
 import com.jojoldu.devbeginnernews.core.article.Article;
 import com.jojoldu.devbeginnernews.core.article.facebook.ArticleFacebookRepository;
-import com.jojoldu.devbeginnernews.core.token.FacebookOauthToken;
-import com.jojoldu.devbeginnernews.core.token.FacebookOauthTokenRepository;
+import com.jojoldu.devbeginnernews.core.token.FacebookPageToken;
+import com.jojoldu.devbeginnernews.core.token.FacebookPageTokenRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -38,11 +38,11 @@ public class TopNewsBatchConfiguration {
     private final FacebookFeedRestTemplate facebookRestTemplate;
     private final ArticleFacebookRepository articleFacebookRepository;
     private final TopNewsJobParameter jobParameter;
-    private final FacebookOauthTokenRepository facebookOauthTokenRepository;
+    private final FacebookPageTokenRepository facebookOauthTokenRepository;
 
     private int chunkSize;
 
-    @Value("${chunkSize:100}")
+    @Value("${chunkSize:1000}")
     public void setChunkSize(int chunkSize) {
         this.chunkSize = chunkSize;
     }
@@ -52,56 +52,56 @@ public class TopNewsBatchConfiguration {
     public TopNewsJobParameter jobParameter() {
         return new TopNewsJobParameter();
     }
-//
+
 //    @Bean(BEAN_PREFIX + "job")
-//    public Job job() {
-//        return jobBuilderFactory.get(JOB_NAME)
-//                .start(step())
-//                .incrementer(new CleanRunIdIncrementer())
-//                .build();
-//    }
-//
-//    @Bean(BEAN_PREFIX + "step")
-//    public Step step() {
-//        return stepBuilderFactory.get(BEAN_PREFIX+ "step")
-//                .<FacebookFeedDto, Article>chunk(chunkSize)
-//                .reader(reader())
-//                .processor(processor())
-//                .writer(writer())
-//                .build();
-//    }
-//
-//    @Bean(BEAN_PREFIX + "reader")
-//    @StepScope
-//    public FacebookPageItemReader reader() {
-//        FacebookOauthToken token = facebookOauthTokenRepository.findByPageId(jobParameter.getPageId())
-//                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 PageId입니다."));
-//
-//        return new FacebookPageItemReader(
-//                facebookRestTemplate,
-//                chunkSize,
-//                token.getPageId(),
-//                token.getToken());
-//    }
-//
-//    @Bean(BEAN_PREFIX + "processor")
-//    @StepScope
-//    public ItemProcessor<FacebookFeedDto, Article> processor() {
-//        return item -> {
-//            String pageId = jobParameter.getPageId();
-//            Article article = item.toArticle(pageId);
-//
-//            return articleFacebookRepository.findByPostsId(article.getPostsId())
-//                    .map(a -> a.updateArticle(article.getTitle(), article.getContent(), article.getLikes()))
-//                    .orElse(article);
-//        };
-//    }
-//
-//    @Bean(BEAN_PREFIX + "writer")
-//    public JpaItemWriter<Article> writer() {
-//        JpaItemWriter<Article> itemWriter = new JpaItemWriter<>();
-//        itemWriter.setEntityManagerFactory(emf);
-//        return itemWriter;
-//    }
+    public Job job() {
+        return jobBuilderFactory.get(JOB_NAME)
+                .start(step())
+                .incrementer(new CleanRunIdIncrementer())
+                .build();
+    }
+
+    @Bean(BEAN_PREFIX + "step")
+    public Step step() {
+        return stepBuilderFactory.get(BEAN_PREFIX+ "step")
+                .<FacebookFeedDto, Article>chunk(chunkSize)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean(BEAN_PREFIX + "reader")
+    @StepScope
+    public FacebookPageItemReader reader() {
+        FacebookPageToken token = facebookOauthTokenRepository.findByPageId(jobParameter.getPageId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 PageId입니다."));
+
+        return new FacebookPageItemReader(
+                facebookRestTemplate,
+                chunkSize,
+                token.getPageId(),
+                token.getToken());
+    }
+
+    @Bean(BEAN_PREFIX + "processor")
+    @StepScope
+    public ItemProcessor<FacebookFeedDto, Article> processor() {
+        return item -> {
+            String pageId = jobParameter.getPageId();
+            Article article = item.toArticle(pageId);
+
+            return articleFacebookRepository.findByPostsId(article.getPostsId())
+                    .map(a -> a.updateArticle(article.getTitle(), article.getContent(), article.getLikes()))
+                    .orElse(article);
+        };
+    }
+
+    @Bean(BEAN_PREFIX + "writer")
+    public JpaItemWriter<Article> writer() {
+        JpaItemWriter<Article> itemWriter = new JpaItemWriter<>();
+        itemWriter.setEntityManagerFactory(emf);
+        return itemWriter;
+    }
 
 }
